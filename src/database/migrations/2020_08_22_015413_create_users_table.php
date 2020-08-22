13b1009dc6e34757552f67149331596fa1667f4d
Hugo Martins <hugo_m_s@msn.com>
2020-08-22 19:39:01 -0300

    Adicionando rede nos containers e criando migrations

---
+++ src/database/migrations/2020_08_22_015413_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 20);
+            $table->string('sobrenome', 255);
+            $table->string('documento');
+            $table->string('email', 255)->unique();
+            $table->string('senha');
+            $table->foreignId('usuario_tipo_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
