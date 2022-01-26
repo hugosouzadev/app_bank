674d740ad356e6a3b29818dd046860bb4bce7777
Hugo Martins de Souza <hugo.souza@picpay.com>
2022-01-25 21:01:34 -0300

    Criando exemplos de consumers

---
+++ src/app/Console/Commands/ConsumerTestDlqCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\Log;
+use Junges\Kafka\Contracts\Consumer;
+use Junges\Kafka\Contracts\KafkaConsumerMessage;
+
+class ConsumerTestDlqCommand extends Consumer
+{
+    public function handle(KafkaConsumerMessage $message): void
+    {
+        Log::info("DLQ ". $message->getBody()['id']);
+    }
+}
